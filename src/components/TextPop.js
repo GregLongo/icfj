02983c8d1868bb726fe52fc05fe4d1c4db897c63
greglongo <gregorylongo@gmail.com>
2021-03-09 20:58:01 -0800

    not sure

--- src/components/TextPop.js
+++ src/components/TextPop.js
@@ -2,35 +2,73 @@ import React, { Component } from "react"
 import styled from "@emotion/styled"
 import { gsap } from "gsap" 
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import {keyframes } from '@emotion/react'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const Pop = styled.span`
-	color: pink;
-`
-
 class TextPop extends Component{
-  
-  	constructor(props){
-        super(props);
-        this.ref = React.createRef();
-        }
-  	 componentDidMount(){
-      ScrollTrigger.create({
-        trigger: this.ref.current,
-        start: 'top center',			
-        markers: true
-      })
-	  }
-    render(){
-      return(
-        <Pop ref={this.ref} className="pop">{this.props.children}</Pop>
-      
-      )
-      
-    }
-  
-  }
 
+	constructor(props){
+		super(props);
+		this.state = {
+			pop: false
+		}
+		this.ref = React.createRef();
+	}
+
+
+	popUp = () => {this.setState({pop:true})}
+	fallDown = () => {this.setState({pop:false})}
+
+	 componentDidMount(){
+		ScrollTrigger.create({
+			trigger: this.ref.current,
+			start: 'top center',			
+			onEnter: this.popUp,
+			onLeave: this.fallDown,
+			markers: false
+		})
+	  }
+
+	render(){
+
+		const color = this.props.color
+
+		const  popAnim = keyframes`
+			50%  {transform: scale(1.2) translate(0px, -2px);
+			text-shadow: 4px 10px 20px ${color}}
+			};
+		`
+		const  popFlare = keyframes`
+			50%  {transform: scale(1.7) translate(0px, -10px) ;
+			text-shadow: 4px 10px 20px ${color};
+			}
+			80% {
+				opacity: 0;
+			}
+			100% {
+				opacity: 0;
+			}
+		`
+
+		const Pop = styled.span`
+			display: inline-block;
+			color: ${color};
+     		animation: ${this.state.pop ? popAnim : ''} 1.2s ease ;
+     		&:before{
+     			content: '${this.props.children}';
+     			position: absolute;
+     			opacity: .3;
+         		animation: ${this.state.pop ? popFlare : ''} 1s ease  			
+     		}
+			`
+
+	return <Pop ref={this.ref}>{this.props.children}</Pop>
+	}
+}
+
+TextPop.defaultProps = {
+	color: 'white'
+}
 
 export default TextPop
